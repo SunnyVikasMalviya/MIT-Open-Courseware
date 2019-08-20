ace79116672cd4eb109d471a72c4566351e38879
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-08-20 21:53:19 +0530

    Update Max_Heapify.py

--- 6.006 Introduction to Algorithms/Max_Heapify.py
+++ 6.006 Introduction to Algorithms/Max_Heapify.py
@@ -2,8 +2,6 @@ def max_heapify(arr, i, n) :
     """
     The max_heapify function makes a max heap out of a heap that has a single
     violation of the max heap property.
-    The list should be have odd number of elements in order to visualize it as
-    a nearly complete binary tree.
     Time complexity of max_heapify is O(log n)
     ***
     Assumption
@@ -15,8 +13,10 @@ def max_heapify(arr, i, n) :
     i = index at which the max heap property is violated.
     n = number of elements in the list.
     """
-    if n % 2 != 1:
-        return "Number of elements should be odd in order to visualize the list heap as a binary tree."
+    if n == (2*i)+2 :
+        if arr[i] < arr[(2*i)+1] :
+            arr[i], arr[(2*i)+1] = arr[(2*i)+1], arr[i]
+        return arr 
     if arr[i] < arr[(2*i)+1] or arr[i] < arr[(2*i)+2] :
         if arr[(2*i)+1] >= arr[(2*i)+2]:
             arr[(2*i)+1], arr[i] = arr[i], arr[(2*i)+1]
@@ -30,15 +30,15 @@ def max_heapify(arr, i, n) :
 
 
 
-#For Debugging
-#print(max_heapify([9, 1, 8, 4, 5, 6, 7, 3, 2], 1, 9))
-
+if __name__ == '__main__' :
+    print(max_heapify([9, 1, 8, 4, 5, 6, 7, 3, 2], 1, 9))
+    print(max_heapify([20, 2, 11, 6, 10, 8, 9, 1, 2, 3, 4, 5], 1, 12))
 
 """
 INPUT
 
-[9, 1, 8, 4, 5, 6, 7, 3, 2]
+[20, 2, 11, 6, 10, 8, 9, 1, 2, 3, 4, 5]
 OUTPUT
 
-[9, 5, 8, 4, 1, 6, 7, 3, 2]
+[20, 10, 11, 6, 4, 8, 9, 1, 2, 3, 2, 5]
 """
